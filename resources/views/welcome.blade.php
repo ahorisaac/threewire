cddb5f0f72b4e93fe7f61c3fe6815bc6bb20fd1f
ahorisaac <ahorisaac.ai@gmail.com>
2024-05-20 22:25:24 +0100

    chore: install flowbite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body class="antialiased">
-        @livewire("clicker")
+        {{-- @livewire("clicker") --}}
+
+        {{-- @livewire('register-form') --}}
     </body>
 </html>
