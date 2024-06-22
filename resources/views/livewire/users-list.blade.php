be7b9e77eed09b6fa930625bf18f6ce6fa71c48b
ahorisaac <ahorisaac.ai@gmail.com>
2024-06-22 22:57:19 +0100

    livewire: polling, refresh component automatically

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -1,4 +1,4 @@
-<div class="px-4">
+<div wire:poll.keep-alive.10s class="px-4">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-auto mt-8 border-[1px] border-solid border-gray-200">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
